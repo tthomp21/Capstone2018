ea1d60e8f99b05fff6d381f993665c50196ef518
radad <radad@DESKTOP-L1P0CQH>
2018-01-21 00:38:18 -0800

    created variables to get clientID, and TreeMap so the list are sorted by date disbursed. ...

--- src/business/AssistanceRequest.java
+++ src/business/AssistanceRequest.java
@@ -36,6 +36,104 @@ public class AssistanceRequest implements Serializable{
 
     }
 
+    /**
+     * @return the anAssistance
+     */
+    public Assitance getAnAssistance() {
+        return anAssistance;
+    }
+
+    /**
+     * @param anAssistance the anAssistance to set
+     */
+    public void setAnAssistance(Assitance anAssistance) {
+        this.anAssistance = anAssistance;
+    }
+
+    /**
+     * @return the assistanceID
+     */
+    public int getAssistanceID() {
+        return assistanceID;
+    }
+
+    /**
+     * @param assistanceID the assistanceID to set
+     */
+    public void setAssistanceID(int assistanceID) {
+        this.assistanceID = assistanceID;
+    }
+
+    /**
+     * @return the client
+     */
+    public Client getClient() {
+        return client;
+    }
+
+    /**
+     * @param client the client to set
+     */
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    /**
+     * @return the requestDate
+     */
+    public LocalDate getRequestDate() {
+        return requestDate;
+    }
+
+    /**
+     * @param requestDate the requestDate to set
+     */
+    public void setRequestDate(LocalDate requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    /**
+     * @return the amountPaid
+     */
+    public double getAmountPaid() {
+        return amountPaid;
+    }
+
+    /**
+     * @param amountPaid the amountPaid to set
+     */
+    public void setAmountPaid(double amountPaid) {
+        this.amountPaid = amountPaid;
+    }
+
+    /**
+     * @return the assistanceStatus
+     */
+    public String getAssistanceStatus() {
+        return assistanceStatus;
+    }
+
+    /**
+     * @param assistanceStatus the assistanceStatus to set
+     */
+    public void setAssistanceStatus(String assistanceStatus) {
+        this.assistanceStatus = assistanceStatus;
+    }
+
+    /**
+     * @return the dateDisbursed
+     */
+    public LocalDate getDateDisbursed() {
+        return dateDisbursed;
+    }
+
+    /**
+     * @param dateDisbursed the dateDisbursed to set
+     */
+    public void setDateDisbursed(LocalDate dateDisbursed) {
+        this.dateDisbursed = dateDisbursed;
+    }
+
 
 
 }
